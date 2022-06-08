b1caa82c6e737f969716796dc7824b4ea28c3ec2
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-08 17:26:30 +0600

    Changing category reducer to typescript

---
+++ src/store/categories/category-action.ts
@@ -0,0 +1,37 @@
+import { CATEGORIES_ACTION_TYPE, Category } from "./category-types";
+
+import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer";
+
+import { getCollectionAndDocuments } from "../../utils/firebase/firebase";
+
+
+export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS, Category[]>;
+
+export type FetchCategoriesFail = ActionWithPayload<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAIL, Error>;
+
+
+export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFail;
+
+export const fetchCategoriesStart = ():FetchCategoriesStart  => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+
+export const fetchCategoriesSuccess = (categoriesArray: Category[]):FetchCategoriesSuccess => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+
+export const fetchCategoriesFail = (error: Error): FetchCategoriesFail => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAIL, error);
+
+
+// export const fetchCategoriesAsync = () => async (dispatch) => {
+
+//     dispatch(fetchCategoriesStart());
+    
+//     try{
+//         const categoriesArray = await getCollectionAndDocuments('categories');
+//         dispatch(fetchCategoriesSuccess(categoriesArray));
+//     }catch(err) {
+//         dispatch(fetchCategoriesFail(err));
+//     }
+    
+// }
--- src/store/categories/category-types.ts
+++ src/store/categories/category-types.ts
@@ -3,3 +3,17 @@ export enum CATEGORIES_ACTION_TYPE  {
     FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS',
     FETCH_CATEGORIES_FAIL = 'FETCH_CATEGORIES_FAIL',
 }
+
+export type CategoryItem = {
+    id: number;
+    imageUrl: string;
+    name: string;
+    price: number;
+}
+
+
+export type Category = {
+    title: string,
+    imageUrl: string;
+    items: 
+}
