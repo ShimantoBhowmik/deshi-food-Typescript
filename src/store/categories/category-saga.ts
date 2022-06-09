a55482dca229368492ba6345bcae8cca7bb290da
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-09 12:59:35 +0600

    Integrated Typecsript to category-saga.ts

---
+++ src/store/categories/category-saga.ts
@@ -0,0 +1,30 @@
+import { takeLatest, all, call, put } from 'typed-redux-saga';
+
+import { getCollectionAndDocuments } from '../../utils/firebase/firebase';
+
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesFail,
+} from './category-action';
+
+import { CATEGORIES_ACTION_TYPE } from './category-types';
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoriesArray = yield* call(getCollectionAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield* put(fetchCategoriesFail(error as Error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield* takeLatest(
+    CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield* all([call(onFetchCategories)]);
+}
