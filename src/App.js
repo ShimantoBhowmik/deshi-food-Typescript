065912ba6cc43992d0648c38abeded57bbd8478b
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-06-10 10:20:11 +0600

    Integrated React lazy and Suspense for better optimaztion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import {  useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import HomePage from './routes/home/homepage';
-import Nav from './routes/Navigation/nav';
-import SignIn from './routes/sign-in/sign-in';
-import CheckOutPage from './routes/checkoutPage/checkoutPage';
-import Shop from './routes/shop/shop';
-import {  useEffect } from "react";
+
 import { checkUserSession } from './store/user/user-action';
+import Spinner from "./components/spinner/spinner";
 
-
+const HomePage = lazy(() => import('./routes/home/homepage'));
+const Nav = lazy(() => import('./routes/Navigation/nav'));
+const Shop = lazy(() => import('./routes/shop/shop'));
+const SignIn = lazy(() => import('./routes/sign-in/sign-in'));
+const CheckOutPage = lazy(() => import('./routes/checkoutPage/checkoutPage'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,14 +21,16 @@ const App = () => {
 
 
   return (
-    <Routes>
-      <Route path='/' element={<Nav />}>
-        <Route index={true} element={<HomePage />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn/>}/>
-        <Route path='checkout' element={<CheckOutPage/>}/>
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner/>}>
+      <Routes>
+        <Route path='/' element={<Nav />}>
+          <Route index={true} element={<HomePage />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='sign-in' element={<SignIn/>}/>
+          <Route path='checkout' element={<CheckOutPage/>}/>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
